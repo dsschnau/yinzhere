2ccb3f4143b69ce2b510b1f9112477cc389159e6
Dan Schnau <dan.schnau@skykick.com>
2021-01-22 22:12:23 -0500

    blah

--- YinzHere/Startup.cs
+++ YinzHere/Startup.cs
@@ -29,6 +29,7 @@ namespace YinzHere
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<IReadyCheckService, ReadyCheckService>();
+            services.AddScoped<ClipboardService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
